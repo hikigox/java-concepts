c19d67241fdb8ea54aaae8d0efc439d017149f8c
Hikigox <jorgealejandroro@gmail.com>
2023-01-29 13:43:52 -0500

    start

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        String hola = "Soy ramon Elias";
+
+        System.out.println(hola);
+    }
+}
---
+++ src/usandoVariablesDeEntorono.java
@@ -0,0 +1,13 @@
+public class usandoVariablesDeEntorono {
+ /*
+ * Esta es una clase donde vamos a ver las diferentes formas de usar las
+ * variables de entorno
+ *
+ *
+ * */
+    public static void main(String[] args) {
+
+
+
+    }
+}
